12e3febf3fff8cf7e79fdd1e0442c1cf01ce5079
PhongT <phongttran01@gmail.com>
2026-06-19 23:53:12 -0400

    Fix heapify and heap_push for complete tree

--- heap_queue.py
+++ heap_queue.py
@@ -1,6 +1,44 @@
 def heapify(arr: list[int]):
-    pass
+    print(f"Starting heapify")
+    print(f"Length of arr: {len(arr)}")
+    for index in range(len(arr) - 1, -1, -1):
+        if (2 * index) + 1 >= len(arr) and (2 * index) + 2 >= len(arr):
+            # leaf node
+            continue
+    
+        left_child = (2 * index) + 1
+        right_child = (2 * index) + 2
 
+        cur_index = index
+
+        # if left child is out of bounds, then right child is also out of bounds
+        while left_child < len(arr):
+            if right_child < len(arr):
+                if arr[left_child] < arr[right_child] and arr[left_child] < arr[cur_index]:
+                    tmp = arr[cur_index]
+                    arr[cur_index] = arr[left_child]
+                    arr[left_child] = tmp
+                    cur_index = left_child
+                    left_child = (2 * cur_index) + 1
+                    right_child = (2 * cur_index) + 2
+                elif arr[right_child] < arr[left_child] and arr[right_child] < arr[cur_index]:
+                    tmp = arr[cur_index]
+                    arr[cur_index] = arr[right_child]
+                    arr[right_child] = tmp
+                    cur_index = right_child
+                    left_child = (2 * cur_index) + 1
+                    right_child = (2 * cur_index) + 2
+                else:
+                    break
+            elif arr[left_child] < arr[cur_index]:
+                tmp = arr[cur_index]
+                arr[cur_index] = arr[left_child]
+                arr[left_child] = tmp
+                cur_index = left_child
+                left_child = (2 * cur_index) + 1
+                right_child = (2 * cur_index) + 2
+            else:
+                break
 
 def heap_push(arr: list[int], value: int) -> None:
     # Assumes arr has been heapified
@@ -11,7 +49,7 @@ def heap_push(arr: list[int], value: int) -> None:
     index = len(arr) - 1
     parent_index = (index - 1) // 2
     
-    while parent_index >= 0 and (arr[parent_index] is None or arr[index] < arr[parent_index]):
+    while parent_index >= 0 and arr[index] < arr[parent_index]:
         # switch new node with parent node
         tmp = arr[index]
         arr[index] = arr[parent_index]
@@ -58,6 +96,10 @@ def heap_peek(arr: list[int]) -> int:
 '''
 Implementing a min-heap.
 
+Attributes:
+- Complete Tree
+- Parent node is smaller than its children
+
 Assumptions
 1. The input is an array representation of a binary tree.
 2. The array is an in-order traversal of the binary tree.
@@ -69,8 +111,12 @@ Given a node at the i-th index, it's parent is located at floor((i - 1) / 2)
 
 '''
 
-arr = [4, None, 9]
-heap_push(arr, 2)
+arr = [10,7,3,5,12,4,0,8,15]
+heapify(arr)
+heap_push(arr, -1)
+heap_push(arr, 20)
+print(arr)
+# heap_push(arr, 2)
 # min_value = heap_pop(arr)
-print(f"arr: {arr}")
+# print(f"arr: {arr}")
 # print(f"min_value: {min_value}")

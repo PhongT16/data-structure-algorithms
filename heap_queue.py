b23fae8f6b44458f523e7bcbc8043bb090d3fab2
PhongT <phongttran01@gmail.com>
2026-06-20 00:34:28 -0400

    Fix heappop

--- heap_queue.py
+++ heap_queue.py
@@ -1,27 +1,92 @@
-def heapify(arr: list[int]):
-    print(f"Starting heapify")
-    print(f"Length of arr: {len(arr)}")
-    for index in range(len(arr) - 1, -1, -1):
-        if (2 * index) + 1 >= len(arr) and (2 * index) + 2 >= len(arr):
-            # leaf node
-            continue
-    
-        left_child = (2 * index) + 1
-        right_child = (2 * index) + 2
+class myHeapQueue:
 
-        cur_index = index
+    @staticmethod
+    def heapify(arr: list[int]):
+        print(f"Starting heapify")
+        print(f"Length of arr: {len(arr)}")
+        for index in range(len(arr) - 1, -1, -1):
+            if (2 * index) + 1 >= len(arr) and (2 * index) + 2 >= len(arr):
+                # leaf node
+                continue
+        
+            left_child = (2 * index) + 1
+            right_child = (2 * index) + 2
 
-        # if left child is out of bounds, then right child is also out of bounds
-        while left_child < len(arr):
-            if right_child < len(arr):
-                if arr[left_child] < arr[right_child] and arr[left_child] < arr[cur_index]:
+            cur_index = index
+
+            # if left child is out of bounds, then right child is also out of bounds
+            while left_child < len(arr):
+                if right_child < len(arr):
+                    if arr[left_child] <= arr[right_child] and arr[left_child] < arr[cur_index]:
+                        tmp = arr[cur_index]
+                        arr[cur_index] = arr[left_child]
+                        arr[left_child] = tmp
+                        cur_index = left_child
+                        left_child = (2 * cur_index) + 1
+                        right_child = (2 * cur_index) + 2
+                    elif arr[right_child] <= arr[left_child] and arr[right_child] < arr[cur_index]:
+                        tmp = arr[cur_index]
+                        arr[cur_index] = arr[right_child]
+                        arr[right_child] = tmp
+                        cur_index = right_child
+                        left_child = (2 * cur_index) + 1
+                        right_child = (2 * cur_index) + 2
+                    else:
+                        break
+                elif arr[left_child] < arr[cur_index]:
                     tmp = arr[cur_index]
                     arr[cur_index] = arr[left_child]
                     arr[left_child] = tmp
                     cur_index = left_child
                     left_child = (2 * cur_index) + 1
                     right_child = (2 * cur_index) + 2
-                elif arr[right_child] < arr[left_child] and arr[right_child] < arr[cur_index]:
+                else:
+                    break
+    @staticmethod
+    def heapppush(arr: list[int], value: int) -> None:
+        # Assumes arr has been heapified
+        if arr is None:
+            raise ValueError("arr cannot be None")
+
+        arr.append(value)
+        index = len(arr) - 1
+        parent_index = (index - 1) // 2
+        
+        while parent_index >= 0 and arr[index] < arr[parent_index]:
+            # switch new node with parent node
+            tmp = arr[index]
+            arr[index] = arr[parent_index]
+            arr[parent_index] = tmp
+
+            index = parent_index
+            parent_index = (index - 1) // 2
+
+    @staticmethod
+    def heappop(arr: list[int]):
+        # Assumes arr has been heapified
+
+        if not arr:
+            raise ValueError("arr is empty. Nothing to pop.")
+        
+        min_number = arr[0]
+        arr[0] = arr[-1]
+        index = 0
+        arr.pop()
+        left_child = (2 * index) + 1
+        right_child = (2 * index) + 2
+
+        cur_index = index
+
+        while left_child < len(arr):
+            if right_child < len(arr):
+                if arr[left_child] <= arr[right_child] and arr[left_child] < arr[cur_index]:
+                    tmp = arr[cur_index]
+                    arr[cur_index] = arr[left_child]
+                    arr[left_child] = tmp
+                    cur_index = left_child
+                    left_child = (2 * cur_index) + 1
+                    right_child = (2 * cur_index) + 2
+                elif arr[right_child] <= arr[left_child] and arr[right_child] < arr[cur_index]:
                     tmp = arr[cur_index]
                     arr[cur_index] = arr[right_child]
                     arr[right_child] = tmp
@@ -39,58 +104,11 @@ def heapify(arr: list[int]):
                 right_child = (2 * cur_index) + 2
             else:
                 break
+        return min_number
 
-def heap_push(arr: list[int], value: int) -> None:
-    # Assumes arr has been heapified
-    if arr is None:
-        raise ValueError("arr cannot be None")
-
-    arr.append(value)
-    index = len(arr) - 1
-    parent_index = (index - 1) // 2
-    
-    while parent_index >= 0 and arr[index] < arr[parent_index]:
-        # switch new node with parent node
-        tmp = arr[index]
-        arr[index] = arr[parent_index]
-        arr[parent_index] = tmp
-
-        index = parent_index
-        parent_index = (index - 1) // 2
-
-def heap_pop(arr: list[int]):
-    # Assumes arr has been heapified
-
-    if not arr:
-        raise ValueError("arr is empty. Nothing to pop.")
-    
-    min_number = arr[0]
-    index = 0
-    left_child = (2 * index) + 1
-    right_child = (2 * index) + 2
-
-    while left_child < len(arr) or right_child < len(arr):
-        if left_child >= len(arr):
-            min_child = right_child
-        elif right_child >= len(arr):
-            min_child = left_child
-        elif arr[left_child] < arr[right_child]:
-            min_child = left_child
-        else:
-            min_child = right_child
-        
-        arr[index] = arr[min_child]
-
-        index = min_child
-        left_child = (2 * index) + 1
-        right_child = (2 * index) + 2
-    
-    # arr.pop(index)
-    arr[index] = None
-    return min_number
-
-def heap_peek(arr: list[int]) -> int:
-    return arr[0]
+    @staticmethod
+    def heappeek(arr: list[int]) -> int:
+        return arr[0]
 
 
 '''
@@ -110,13 +128,3 @@ child is at 2i + 2.
 Given a node at the i-th index, it's parent is located at floor((i - 1) / 2)
 
 '''
-
-arr = [10,7,3,5,12,4,0,8,15]
-heapify(arr)
-heap_push(arr, -1)
-heap_push(arr, 20)
-print(arr)
-# heap_push(arr, 2)
-# min_value = heap_pop(arr)
-# print(f"arr: {arr}")
-# print(f"min_value: {min_value}")
